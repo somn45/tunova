ef4b17e95fe429eb562594625c8537dea16c3183
somn45 <somnsomn45@gmail.com>
2026-07-04 13:37:31 +0900

    feat : Supabase 패키지 설치 및 연동 확인용 쿼리 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
+import { createClient } from "@/libs/supabase/server";
+
+export default async function Home() {
+  const supabase = await createClient();
+  const response = await supabase.from("goods").select();
+  console.log(response.data);
+
   return <div>Hello World</div>;
 }
