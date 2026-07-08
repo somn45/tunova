fb706494fb0a1cee6dc298efd9ed4a26dbeae677
somn45 <somnsomn45@gmail.com>
2026-07-08 22:54:08 +0900

    test : jest 패키지 삭제 및 vitest 패키지 추가 및 설정 적용

--- tests/Home.test.tsx
+++ tests/Home.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import Home from "@/app/page";
 
-it("jest 환경 세팅 적용 테스트", async () => {
+test("jest 환경 세팅 적용 테스트", async () => {
   const homeComponent = Home();
   render(homeComponent);
 });
