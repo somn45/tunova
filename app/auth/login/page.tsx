7ef7757cd1e1a0300f469d2bae314f142d059ec1
somn45 <somnsomn45@gmail.com>
2026-07-08 22:07:28 +0900

    feat : 회원가입과 로그인 페이지에 각각 로그인, 회원가입 페이지로 이동하는 Link 추가

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import LoginForm from "./_Form";
 
 export default function Login() {
   return (
     <div>
       <LoginForm />
+      <Link href="/auth/signup">회원가입 페이지로 이동</Link>
     </div>
   );
 }
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import SignupForm from "./_Form";
 
 export default function Signup() {
   return (
     <div>
       <SignupForm />
+      <Link href="/auth/login">로그인 페이지로 이동</Link>
     </div>
   );
 }
