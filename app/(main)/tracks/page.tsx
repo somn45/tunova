f64a9332f4b468c205947104735b00f3fc612706
somn45 <somnsomn45@gmail.com>
2026-07-12 17:48:16 +0900

    feat : 간단한 랜딩 페이지 내 홈페이지 설명 및 tracks 페이지로 이동하는 Link 추가

---
+++ app/(main)/tracks/page.tsx
@@ -0,0 +1,3 @@
+export default function Tracks() {
+  return <div>트랙 페이지</div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,15 @@
-import { createClient } from "@/libs/supabase/server";
-import OpenAI from "openai";
-
-const OPENAI_GPT_MODEL = "gpt-4.1-mini-2025-04-14";
-const YOUTUBE_API = "https://www.googleapis.com/youtube/v3/search";
-
-interface YTVideo {
-  kind: string;
-  id: {
-    kind: string;
-    videoId: string;
-  };
-  snippet: {
-    channelTitle: string;
-    title: string;
-  };
-}
-
-interface getSearchYTVideo {
-  kind: string;
-  items: Array<YTVideo>;
-  nextPageToken: string;
-}
+import Link from "next/link";
 
 export default async function Home() {
-  const supabase = await createClient();
-  const response = await supabase.from("profiles").select();
-
-  /* Youtube API 연결 테스트
-  const youtubeApiParams = {
-    q: "APT",
-    part: "snippet",
-    key: process.env.NEXT_PUBLIC_YOUTUBE_API_KEY ?? "",
-  };
-  const ytSearchParams = new URLSearchParams(youtubeApiParams).toString();
-
-  const getVideoResponse = await fetch(`${YOUTUBE_API}?${ytSearchParams}`);
-  const getVideoResult: getSearchYTVideo = await getVideoResponse.json();
-
-  const officialRxp = /official/i; // 공식 영상을 가져오도록 정규식 추가
-  const [officialVideo] = getVideoResult.items.filter(video =>
-    officialRxp.exec(video.snippet.title),
+  return (
+    <div>
+      <h1>좋아하는 장르와 아티스트를 조합해 보세요.</h1>
+      <p>
+        복잡한 검색 없이, 당신이 선호하는 음악 장르와 아티스트 데이터를 기반으로
+        취향에 딱 맞는 트랙 목록을 실시간으로 가져옵니다. 탭 몇 번으로 당신이
+        좋아할 만한 새로운 음악들의 리스트를 확인하세요.
+      </p>
+      <Link href="/tracks">지금 시작하기</Link>
+    </div>
   );
-  const vidoeId = officialVideo.id.videoId;
-  */
-
-  /* OpenAI API 연결 테스트
-  const client = new OpenAI();
-  
-  const openAIResponse = await client.responses.create({
-    model: OPENAI_GPT_MODEL,
-    input: "Open AI 연결 테스트입니다. 가볍게 인사해주세요.",
-  });
-
-  console.log(openAIResponse.output_text);
-  */
-  return <div></div>;
 }
